e2483f327b4cab48f9d682e0f88b1712307da330
Maximiliano Sinopoli <m_sinopoli@hotmail.com>
2022-01-14 11:32:22 +0000

    Api fetch succesfully

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,28 +1,26 @@
-import { useEffect, useState } from 'react'
-import Button from '../Button'
-import PokemonCard from '../PokemonCard'
-import WinnerText from '../WinnerText'
-import "./App.css"
+import { useEffect, useState } from "react";
+import Button from "../Button";
+import PokemonCard from "../PokemonCard";
+import WinnerText from "../WinnerText";
+import "./App.css";
 
-function App()
-{
-  const [ pokemonId, setPokemonId ] = useState(1)
-  function handleClick()
-  {
-    let randomNumber = Math.ceil(Math.random() * 151)
-    setPokemonId(randomNumber)
-    console.log(randomNumber)
-
+function App() {
+  const [pokemonId, setPokemonId] = useState(1);
+  function handleClick() {
+    let randomNumber = Math.ceil(Math.random() * 151);
+    setPokemonId(randomNumber);
+    console.log(randomNumber);
   }
 
   return (
     <div className="App">
-      <Button handleClick={handleClick} text='Fetch pokemon' />
-      <WinnerText text='Winner Pokemon' />
+      <Button handleClick={handleClick} text="Fetch pokemon" />
+      <WinnerText text="Winner Pokemon" />
       <PokemonCard id={pokemonId} />
-      <Button text='Compare pokemon' />
+      <PokemonCard id={pokemonId} />
+      <Button text="Compare pokemon" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,29 +1,31 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-function PokemonCard({ name, image, hp, attack, defence, id })
-{
-  const [ pokemon, setPokemon ] = useState(null)
-  useEffect(() =>
-  {
-    async function fetchPokemon()
-    {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      const data = await response.json()
-      console.log('pokemon: ', data)
+function PokemonCard({ id }) {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      const data = await response.json();
+      console.log("pokemon: ", data);
+      setPokemon(data);
     }
-    fetchPokemon()
-  }, [ id ])
+    fetchPokemon();
+  }, [id]);
+  if (!pokemon) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <section className="pokemon__card">
-      <h2>{name}</h2>
-      <img src={image} alt={name} />
+      <h2>NAME:{pokemon.name}</h2>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
       <section className="pokemon__stats">
-        <p>{hp}</p>
-        <p>{attack}</p>
-        <p>{defence}</p>
+        <p>HP={pokemon.stats[0].base_stat}</p>
+        <p>ATTACK={pokemon.stats[1].base_stat}</p>
+        <p>DEFENSE={pokemon.stats[2].base_stat}</p>
       </section>
     </section>
-  )
+  );
 }
 
-export default PokemonCard
+export default PokemonCard;
