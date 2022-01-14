fc22640b9c645b9079a3f4a0032e8b62e386b2ca
Macsen Sully-Morgan <macsullymorgan@gmail.com>
2022-01-14 12:15:23 +0000

    pokemon cards with different ids

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,22 +7,52 @@ import "./App.css";
 let randomNumber = Math.ceil(Math.random() * 151);
 
 function App() {
-  const [pokemonId1, setPokemonId1] = useState(1);
-  const [pokemonId2, setPokemonId2] = useState(randomNumber);
+  const [id1, setId1] = useState(1);
+  const [id2, setId2] = useState(randomNumber);
+  const [pokemon1, setPokemon1] = useState(null);
+  const [pokemon2, setPokemon2] = useState(null);
 
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id1}`);
+      const data = await response.json();
+      console.log("pokemon: ", data);
+      setPokemon1(data);
+    }
+    fetchPokemon();
+  }, [id1]);
+  
+  useEffect(() => {
+    async function fetchPokemon2() {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id2}`);
+      const data = await response.json();
+      console.log("pokemon: ", data);
+      setPokemon2(data);
+    }
+    fetchPokemon2();
+  }, [id2]);
+  
   function handleClick() {
     let randomNumber = Math.ceil(Math.random() * 151);
-    setPokemonId1(randomNumber);
-    setPokemonId2(randomNumber + 1);
+    setId1(randomNumber);
+    setId2(randomNumber + 1);
     console.log(randomNumber);
   }
+  
+  if (!pokemon1) {
+    return <h2>Loading...</h2>;
+  }
 
+  if (!pokemon2) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <div className="App">
       <Button handleClick={handleClick} text="Fetch pokemon" />
       <WinnerText text="Winner Pokemon" />
-      <PokemonCard id={pokemonId1} />
-      <PokemonCard id={pokemonId2} />
+      <PokemonCard id={id1} name={pokemon1.name} src={pokemon1.sprites.front_default} hp={pokemon1.stats[0].base_stat} attack={pokemon1.stats[1].base_stat} defence={pokemon1.stats[2].base_stat}/>
+      <PokemonCard id={id2} name={pokemon2.name} src={pokemon2.sprites.front_default} hp={pokemon2.stats[0].base_stat} attack={pokemon2.stats[1].base_stat} defence={pokemon2.stats[2].base_stat}/>
       <Button text="Compare pokemon" />
     </div>
   );
--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,28 +1,15 @@
 import { useEffect, useState } from "react";
 
-function PokemonCard({ id }) {
-  const [pokemon, setPokemon] = useState(null);
-
-  useEffect(() => {
-    async function fetchPokemon() {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-      const data = await response.json();
-      console.log("pokemon: ", data);
-      setPokemon(data);
-    }
-    fetchPokemon();
-  }, [id]);
-  if (!pokemon) {
-    return <h2>Loading...</h2>;
-  }
+function PokemonCard({name, src, hp, attack, defence}) {
+ 
   return (
     <section className="pokemon__card">
-      <h2>NAME:{pokemon.name}</h2>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      <h2>NAME:{name}</h2>
+      <img src={src} alt={name} />
       <section className="pokemon__stats">
-        <p>HP={pokemon.stats[0].base_stat}</p>
-        <p>ATTACK={pokemon.stats[1].base_stat}</p>
-        <p>DEFENSE={pokemon.stats[2].base_stat}</p>
+        <p>HP={hp}</p>
+        <p>ATTACK={attack}</p>
+        <p>DEFENSE={defence}</p>
       </section>
     </section>
   );
