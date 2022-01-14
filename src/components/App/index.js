07d10c7c7e83dfc27d6e0fdb1051394609505fe5
Maximiliano Sinopoli <m_sinopoli@hotmail.com>
2022-01-14 13:56:02 +0000

    Abilities added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,101 +1,117 @@
-import { useEffect, useState } from "react"
-import Button from "../Button"
-import PokemonCard from "../PokemonCard"
-import WinnerText from "../WinnerText"
-import "./App.css"
+import { useEffect, useState } from "react";
+import Button from "../Button";
+import PokemonCard from "../PokemonCard";
+import WinnerText from "../WinnerText";
+import "./App.css";
 
-let randomNumber = Math.ceil(Math.random() * 151)
+let randomNumber = Math.ceil(Math.random() * 151);
 
-function App()
-{
-  const [ id1, setId1 ] = useState(1)
-  const [ id2, setId2 ] = useState(randomNumber)
-  const [ pokemon1, setPokemon1 ] = useState(null)
-  const [ pokemon2, setPokemon2 ] = useState(null)
-  const [ message, setMessage ] = useState("Compare Pokemon to see which will will in a fight!")
+function App() {
+  const [id1, setId1] = useState(1);
+  const [id2, setId2] = useState(randomNumber);
+  const [pokemon1, setPokemon1] = useState(null);
+  const [pokemon2, setPokemon2] = useState(null);
+  const [message, setMessage] = useState(
+    "Compare Pokemon to see which will will in a fight!"
+  );
 
-
-  useEffect(() =>
-  {
-    async function fetchPokemon()
-    {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id1}`)
-      const data = await response.json()
+  useEffect(() => {
+    async function fetchPokemon() {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id1}`);
+      const data = await response.json();
       // console.log("pokemon: ", data)
-      setPokemon1(data)
+      setPokemon1(data);
       // console.log('pokemon1:', data.stats[ 1 ].base_stat)
     }
-    fetchPokemon()
-  }, [ id1 ])
+    fetchPokemon();
+  }, [id1]);
 
-  useEffect(() =>
-  {
-    async function fetchPokemon2()
-    {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id2}`)
-      const data = await response.json()
-      // console.log("pokemon: ", data)
-      setPokemon2(data)
+  useEffect(() => {
+    async function fetchPokemon2() {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id2}`);
+      const data = await response.json();
+      console.log("pokemon: ", data.abilities);
+      setPokemon2(data);
       // console.log('pokemon2:', data.stats[ 1 ].base_stat)
-
     }
-    fetchPokemon2()
-  }, [ id2 ])
+    fetchPokemon2();
+  }, [id2]);
 
-  function handleClick()
-  {
-    let randomNumber1 = Math.ceil(Math.random() * 151)
-    let randomNumber2 = Math.ceil(Math.random() * 151)
-    setId1(randomNumber1)
-    setId2(randomNumber2)
+  function handleClick() {
+    let randomNumber1 = Math.ceil(Math.random() * 151);
+    let randomNumber2 = Math.ceil(Math.random() * 151);
+    setId1(randomNumber1);
+    setId2(randomNumber2);
+    setMessage("Compare Pokemon to see which will will in a fight!");
     // console.log(randomNumber1)
     // console.log(randomNumber2)
   }
-  function comparePokemon()
-  {
-    let sumP1 = pokemon1.stats[ 0 ].base_stat + pokemon1.stats[ 1 ].base_stat + pokemon1.stats[ 2 ].base_stat
-    let sumP2 = pokemon2.stats[ 0 ].base_stat + pokemon2.stats[ 1 ].base_stat + pokemon2.stats[ 2 ].base_stat
-    if (sumP1 < sumP2)
-    {
-
-      console.log('P2 won', sumP1, sumP2)
-      setMessage(`${pokemon2.name} won`)
-
+  function comparePokemon() {
+    let sumP1 =
+      pokemon1.stats[0].base_stat +
+      pokemon1.stats[1].base_stat +
+      pokemon1.stats[2].base_stat;
+    let sumP2 =
+      pokemon2.stats[0].base_stat +
+      pokemon2.stats[1].base_stat +
+      pokemon2.stats[2].base_stat;
+    if (sumP1 < sumP2) {
+      console.log("P2 won", sumP1, sumP2);
+      setMessage(`${pokemon2.name} won`);
     }
-    if (sumP1 > sumP2)
-    {
-
-      console.log('P1 won', sumP1, sumP2)
-      setMessage(`${pokemon1.name} won`)
-
-
-    } else if (sumP1 === sumP2)
-    {
-      console.log('Draw', sumP1, sumP2)
-      setMessage(`${pokemon1.name} and ${pokemon2.name} have the same strength`)
-
+    if (sumP1 > sumP2) {
+      console.log("P1 won", sumP1, sumP2);
+      setMessage(`${pokemon1.name} won`);
+    } else if (sumP1 === sumP2) {
+      console.log("Draw", sumP1, sumP2);
+      setMessage(
+        `${pokemon1.name} and ${pokemon2.name} have the same strength`
+      );
     }
   }
 
-  if (!pokemon1)
-  {
-    return <h2>Loading...</h2>
+  if (!pokemon1) {
+    return <h2>Loading...</h2>;
   }
 
-  if (!pokemon2)
-  {
-    return <h2>Loading...</h2>
+  if (!pokemon2) {
+    return <h2>Loading...</h2>;
   }
   return (
     <div className="App">
       <Button handleClick={handleClick} text="Fetch pokemon" />
       <WinnerText text={message} />
-      <PokemonCard id={id1} name={pokemon1.name} src={pokemon1.sprites.front_default} hp={pokemon1.stats[ 0 ].base_stat} attack={pokemon1.stats[ 1 ].base_stat} defence={pokemon1.stats[ 2 ].base_stat} />
-      <PokemonCard id={id2} name={pokemon2.name} src={pokemon2.sprites.front_default} hp={pokemon2.stats[ 0 ].base_stat} attack={pokemon2.stats[ 1 ].base_stat} defence={pokemon2.stats[ 2 ].base_stat} />
+      <PokemonCard
+        id={id1}
+        name={pokemon1.name}
+        src={pokemon1.sprites.front_default}
+        strength={
+          pokemon1.stats[0].base_stat +
+          pokemon1.stats[1].base_stat +
+          pokemon1.stats[2].base_stat
+        }
+        abilities={pokemon1.abilities}
+        hp={pokemon1.stats[0].base_stat}
+        attack={pokemon1.stats[1].base_stat}
+        defence={pokemon1.stats[2].base_stat}
+      />
+      <PokemonCard
+        id={id2}
+        name={pokemon2.name}
+        src={pokemon2.sprites.front_default}
+        strength={
+          pokemon2.stats[0].base_stat +
+          pokemon2.stats[1].base_stat +
+          pokemon2.stats[2].base_stat
+        }
+        abilities={pokemon1.abilities}
+        hp={pokemon2.stats[0].base_stat}
+        attack={pokemon2.stats[1].base_stat}
+        defence={pokemon2.stats[2].base_stat}
+      />
       <Button handleClick={comparePokemon} text="Compare pokemon" />
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 
-function PokemonCard({name, src, hp, attack, defence}) {
- 
+function PokemonCard({ name, src, strength, abilities }) {
   return (
     <section className="pokemon__card">
       <h2>NAME:{name}</h2>
       <img src={src} alt={name} />
       <section className="pokemon__stats">
-        <p>HP={hp}</p>
-        <p>ATTACK={attack}</p>
-        <p>DEFENSE={defence}</p>
+        <p>STRENGTH={strength}</p>
+        <ul>
+          {abilities.map((item) => {
+            return <li key={item.ability.name}>{item.ability.name}</li>;
+          })}
+        </ul>
       </section>
     </section>
   );
