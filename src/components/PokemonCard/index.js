2f57ab39b7d6e7808702673131e2dc67d40f4ee6
21ciprian <2021ciprianoffice@gmail.com>
2022-01-14 15:19:57 +0000

    Capitalized the name and abilities

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,21 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 import "./Pokemoncard.css"
 
-function PokemonCard({ name, src, strength, abilities }) {
+function PokemonCard({ name, src, strength, abilities })
+{
   return (
     <section className="pokemon__card">
-      <h2>NAME:{name}</h2>
+      <h2>{name[ 0 ].toUpperCase() + name.slice(1).toLowerCase()}</h2>
       <img src={src} alt={name} />
       <section className="pokemon__stats">
-        <p>STRENGTH={strength}</p>
+        <p>Strength:{strength}</p>
         <ul>
-          {abilities.map((item) => {
-            return <li key={item.ability.name}>{item.ability.name}</li>;
+          {abilities.map((item) =>
+          {
+            return <li key={item.ability.name}>{item.ability.name[ 0 ].toUpperCase() + name.slice(1).toLowerCase()}</li>
           })}
         </ul>
       </section>
     </section>
-  );
+  )
 }
 
-export default PokemonCard;
+export default PokemonCard
